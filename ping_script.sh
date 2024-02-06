4fc7bb961b26fdbc2ba4236660273dbf303f8a1c
labanda1 <lesmorother@hotmail.com>
2024-02-06 17:44:29 -0500

    Updated ping file

--- ping_script.sh
+++ ping_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Copied to my Queuerepo 
 # Array of IP addresses
 ip_addresses=("192.168.1.1" "192.168.1.200" "192.168.1.198" "192.168.1.104" "192.168.1.5" "192.168.1.6" "192.168.1.7")
 
